ed0ad8c276709bfe4ed219ab73f16e7915526004
SS47816 <ss1057532022@gmail.com>
2021-12-26 17:06:43 +0800

    feat: add box.hpp

---
+++ include/lidar_obstacle_detector/box.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Eigen/Geometry> 
+
+struct Box
+{
+ public:
+	int id;
+	int color;
+	float x_min;
+	float y_min;
+	float z_min;
+	float x_max;
+	float y_max;
+	float z_max;
+};
+
+struct BoxQ
+{
+ public:
+  int id;
+	Eigen::Vector3f bboxTransform;
+	Eigen::Quaternionf bboxQuaternion;
+	float cube_length;
+  float cube_width;
+  float cube_height;
+};
